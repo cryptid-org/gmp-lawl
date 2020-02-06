7082bdb255555ea70bff1ee76f730f246380a4c4
bombifighter <botond20@gmail.com>
2020-02-06 04:53:31 +0100

    Removed remove, roinit_n, root, rootrem, rrandomb from mpz

--- compat.c
+++ compat.c
@@ -49,12 +49,3 @@ __MPN (divmod_1) (mp_ptr dst, mp_srcptr src, mp_size_t size, mp_limb_t divisor)
 {
   return mpn_divmod_1 (dst, src, size, divisor);
 }
-
-
-/* mpz_legendre was a separate function in gmp 3.1.1 and earlier, but as of
-   4.0 it's a #define alias for mpz_jacobi.  */
-int
-__gmpz_legendre (mpz_srcptr a, mpz_srcptr b)
-{
-  return mpz_jacobi (a, b);
-}
